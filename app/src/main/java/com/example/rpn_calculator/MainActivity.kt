bd96d826ed6245f65df45cffb42b0c48bc27260d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-15 17:17:00 +0200

    use simple button to test add to stack command

--- app/src/main/java/com/example/rpn_calculator/MainActivity.kt
+++ app/src/main/java/com/example/rpn_calculator/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.rpn_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rpn_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val stack: ArrayDeque<Int> = ArrayDeque()
     private val commandHandler = CalculatorCommandHandler()
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            commandHandler.handle(AddToStackCalculatorCommand(stack, 5))
+            println(stack)
+        }
     }
 }

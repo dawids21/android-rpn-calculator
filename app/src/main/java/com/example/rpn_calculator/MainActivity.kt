e433b500243970be849b9e73d8bf8d68b44ba240
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-15 17:04:00 +0200

    instantiate handler and stack in main activity

--- app/src/main/java/com/example/rpn_calculator/MainActivity.kt
+++ app/src/main/java/com/example/rpn_calculator/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val stack: ArrayDeque<Int> = ArrayDeque()
+    private val commandHandler = CalculatorCommandHandler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

ccf8354f6604f3cbb4b830e48fc63f8811f492fe
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-15 17:57:23 +0200

    create RemoveStackCalculatorCommand.kt

---
+++ app/src/main/java/com/example/rpn_calculator/RemoveStackCalculatorCommand.kt
@@ -0,0 +1,18 @@
+package com.example.rpn_calculator
+
+class RemoveStackCalculatorCommand(private val stack: ArrayDeque<Double>) : CalculatorCommand {
+
+    private var number = 0.0
+
+    override fun execute(): CommandResult {
+        if (stack.count() < 1) {
+            return FailureCommandResult("Not enough numbers on the stack")
+        }
+        number = stack.removeLast()
+        return SuccessCommandResult()
+    }
+
+    override fun undo() {
+        stack.addLast(number)
+    }
+}

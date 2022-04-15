4b58c6dc0e578b4a18870a0c354005a5a5ea0a72
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-15 18:19:29 +0200

    create NegateCalculatorCommand.kt

---
+++ app/src/main/java/com/example/rpn_calculator/NegateCalculatorCommand.kt
@@ -0,0 +1,18 @@
+package com.example.rpn_calculator
+
+class NegateCalculatorCommand(private val stack: ArrayDeque<Double>) : CalculatorCommand {
+
+    override fun execute(): CommandResult {
+
+        if (stack.count() < 1) {
+            return FailureCommandResult("Not enough numbers on the stack")
+        }
+
+        stack.addLast(-stack.removeLast())
+        return SuccessCommandResult()
+    }
+
+    override fun undo() {
+        stack.addLast(-stack.removeLast())
+    }
+}

923e343f1ee59cca5fd4e7f0b4cc246f9d8a7ba7
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-15 17:43:24 +0200

    create AddCalculatorCommand.kt

---
+++ app/src/main/java/com/example/rpn_calculator/AddCalculatorCommand.kt
@@ -0,0 +1,24 @@
+package com.example.rpn_calculator
+
+class AddCalculatorCommand(private val stack: ArrayDeque<Int>) : CalculatorCommand {
+
+    private var a = 0
+    private var b = 0
+
+    override fun execute(): CommandResult {
+        if (stack.count() < 2) {
+            return FailureCommandResult("Not enough numbers on the stack")
+        }
+
+        a = stack.removeLast()
+        b = stack.removeLast()
+        stack.addLast(a + b)
+        return SuccessCommandResult()
+    }
+
+    override fun undo() {
+        stack.removeLast()
+        stack.addLast(b)
+        stack.addLast(a)
+    }
+}

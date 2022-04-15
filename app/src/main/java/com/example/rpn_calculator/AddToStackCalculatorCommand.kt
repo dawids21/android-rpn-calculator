b58e97496608cba8eb062edd4ffcf67338d2b787
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-15 17:02:23 +0200

    create AddToStackCalculatorCommand.kt

---
+++ app/src/main/java/com/example/rpn_calculator/AddToStackCalculatorCommand.kt
@@ -0,0 +1,13 @@
+package com.example.rpn_calculator
+
+class AddToStackCalculatorCommand(private val stack: ArrayDeque<Int>, private val number: Int) : CalculatorCommand {
+
+    override fun execute(): CommandResult {
+        stack.addLast(number)
+        return SuccessCommandResult()
+    }
+
+    override fun undo() {
+        stack.removeLast()
+    }
+}

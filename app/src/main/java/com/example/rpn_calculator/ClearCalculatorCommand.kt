9307f83a39056430ab6f1ef34293414877570e64
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-15 18:15:55 +0200

    create ClearCalculatorCommand.kt

---
+++ app/src/main/java/com/example/rpn_calculator/ClearCalculatorCommand.kt
@@ -0,0 +1,15 @@
+package com.example.rpn_calculator
+
+class ClearCalculatorCommand(private val stack: ArrayDeque<Double>) : CalculatorCommand {
+
+    private val clearedNumbers: ArrayList<Double> = ArrayList()
+
+    override fun execute(): CommandResult {
+        stack.forEach { clearedNumbers.add(it) }
+        return SuccessCommandResult()
+    }
+
+    override fun undo() {
+        clearedNumbers.forEach { stack.addLast(it) }
+    }
+}

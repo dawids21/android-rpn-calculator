bb233b9767897541ad030b110e6fce58883b6686
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-16 16:43:49 +0200

    handle back button on settings activity

--- app/src/main/java/com/example/rpn_calculator/SettingsActivity.kt
+++ app/src/main/java/com/example/rpn_calculator/SettingsActivity.kt
@@ -2,10 +2,20 @@ package com.example.rpn_calculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.rpn_calculator.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            finish()
+        }
     }
 }

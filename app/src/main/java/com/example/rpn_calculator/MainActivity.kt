1fecc3adf6f2738cc36e92a39623a30b23e22351
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-16 15:04:04 +0200

    handle editor buttons

--- app/src/main/java/com/example/rpn_calculator/MainActivity.kt
+++ app/src/main/java/com/example/rpn_calculator/MainActivity.kt
@@ -23,6 +23,18 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val numberButtons = listOf(
+            binding.zeroButton, binding.oneButton, binding.twoButton, binding.threeButton,
+            binding.fourButton, binding.fiveButton, binding.sixButton, binding.sevenButton,
+            binding.eightButton, binding.nineButton
+        )
+
+        for ((index, button) in numberButtons.withIndex()) {
+            button.setOnClickListener { putInEditor(index.toString()) }
+        }
+
+        binding.dotButton.setOnClickListener { putInEditor(".") }
+
         binding.enterButton.setOnClickListener {
             val command = AddToStackCalculatorCommand(stack, 5.0)
             Log.i(COMMAND_TAG, "Sending $command")
@@ -35,5 +47,17 @@ class MainActivity : AppCompatActivity() {
             commandHandler.undo()
             Log.i(STACK_TAG, "Current stack: $stack")
         }
+    }
+
+    private fun putInEditor(text: String) {
+
+        val editorView = binding.editorView
+        if (text == "0" && editorView.text.isEmpty()) {
+            return
+        }
+        if (text == "." && (editorView.text.isBlank() || editorView.text.contains("."))) {
+            return
+        }
+        editorView.append(text)
     }
 }

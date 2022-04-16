d77fc5e781529b72d9236556df69054cff9afd1b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-16 22:40:34 +0200

    handle apply button

--- app/src/main/java/com/example/rpn_calculator/SettingsActivity.kt
+++ app/src/main/java/com/example/rpn_calculator/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.rpn_calculator
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,18 +18,19 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
         title = "Settings"
 
+        val applyButton = findViewById<Button>(R.id.applySettingsButton)
+        applyButton.setOnClickListener {
+            val data = Intent()
+            val numberInput = findViewById<TextView>(R.id.decimalPlacesInput)
+            if (numberInput.text.isNotBlank()) {
+                data.putExtra(DECIMAL_PLACES_EXTRA_NAME, numberInput.text.toString().toInt())
+            }
+            setResult(Activity.RESULT_OK, data)
+            finish()
+        }
+
         val extras = intent.extras ?: return
         val numberInput = findViewById<TextView>(R.id.decimalPlacesInput)
         numberInput.text = extras.getInt(DECIMAL_PLACES_EXTRA_NAME).toString()
-    }
-
-    override fun finish() {
-
-        val data = Intent()
-        val numberInput = findViewById<TextView>(R.id.decimalPlacesInput)
-        if (numberInput.text.isNotBlank()) {
-            data.putExtra(DECIMAL_PLACES_EXTRA_NAME, numberInput.text.toString().toInt())
-        }
-        super.finish()
     }
 }

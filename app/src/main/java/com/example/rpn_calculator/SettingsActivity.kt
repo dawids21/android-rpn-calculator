b8236aeb3e32f4070071eea985f3c1f9d6e6d228
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-16 22:05:54 +0200

    remove view binding from settings activity

--- app/src/main/java/com/example/rpn_calculator/SettingsActivity.kt
+++ app/src/main/java/com/example/rpn_calculator/SettingsActivity.kt
@@ -2,17 +2,12 @@ package com.example.rpn_calculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.rpn_calculator.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySettingsBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySettingsBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_settings)
         title = "Settings"
     }
 }
